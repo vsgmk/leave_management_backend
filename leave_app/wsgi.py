54de5e033b0af27bc0158b48fb26d8826eb59396
Vaibav Survase <vaibhavsurvase674@gmail.com>
2025-04-11 21:33:29 +0530

    port added

--- leave_app/wsgi.py
+++ leave_app/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+port = os.getenv("PORT", "8000")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leave_app.settings')
 
 application = get_wsgi_application()
