d5998a9be0f990982f1fa1f5a29c9970608af568
Vaibav Survase <vaibhavsurvase674@gmail.com>
2025-04-11 22:07:55 +0530

    Fix: Add Render domain to ALLOWED_HOSTS

--- leave_app/settings.py
+++ leave_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+_cdj0lt=^x4cez9r&%qz&gp)vs+f$)k2iuwag4sg-oad_cw%3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['leave-management-backend-3.onrender.com']
 
 
 # Application definition
